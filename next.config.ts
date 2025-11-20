864ddb93bcc8adf0334353745f2387ddbc4f5503
Kirill Mikhailov <137108844+KirillMikhailov442@users.noreply.github.com>
2025-11-20 18:58:51 +0500

    fix: fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    reactStrictMode: false,
-    experimental: {
-        webpackMemoryOptimizations: true,
-        preloadEntriesOnStart: false,
-        turbo: {
-            memoryLimit: 512 * 1024 * 1024,
-        },
-    },
-    webpack: (config, { dev }) => {
-        if (config.cache && !dev) {
-            config.cache = Object.freeze({
-                type: 'memory',
-            });
-        }
-
-        return config;
-    },
+  reactStrictMode: false,
+  experimental: {
+    webpackMemoryOptimizations: true,
+    preloadEntriesOnStart: false,
+  },
 };
 
 export default nextConfig;
