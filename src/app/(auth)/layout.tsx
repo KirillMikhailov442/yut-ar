b8912b2fc78832029e86a7812023f3677fffd84d
Kirill Mikhailov <137108844+KirillMikhailov442@users.noreply.github.com>
2025-12-06 16:34:58 +0500

    fix: pre-release

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
-import { Provider } from "@/components/ui/provider";
-import { Theme } from "@chakra-ui/react";
-import Link from "next/link";
+import { Provider } from '@/components/ui/provider';
+import { Theme } from '@chakra-ui/react';
+import Link from 'next/link';
 
 const AuthLayout = ({
   children,
@@ -9,16 +9,14 @@ const AuthLayout = ({
 }>) => {
   return (
     <Provider>
-      <Theme appearance="light">
-        <div className="h-[100vh] flex flex-col">
-          <header className="flex">
-            <Link href={"/"} className="auth-back">
-              НА ГЛАВНУЮ
-            </Link>
-          </header>
-          <div className="flex-grow flex p-8 max-lg:p-4 !mx-4">{children}</div>
-        </div>
-      </Theme>
+      <div className="h-[100vh] flex flex-col">
+        <header className="flex">
+          <Link href={'/'} className="auth-back">
+            НА ГЛАВНУЮ
+          </Link>
+        </header>
+        <div className="flex-grow flex p-8 max-lg:p-4 !mx-4">{children}</div>
+      </div>
     </Provider>
   );
 };
--- src/components/layouts/Second/index.tsx
+++ src/components/layouts/Second/index.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import styles from "./Second.module.scss";
-import Link from "next/link";
-import Image from "next/image";
-import user_img from "@images/user.webp";
-import { Provider } from "@/components/ui/provider";
-import AddProjectModal from "@/components/Modals/AddProject";
-import Cookies from "js-cookie";
-import { IUser } from "@/types/User";
-import { LightMode } from "@components/ui/color-mode";
+import React, { useEffect, useState } from 'react';
+import styles from './Second.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import user_img from '@images/user.webp';
+import { Provider } from '@/components/ui/provider';
+import AddProjectModal from '@/components/Modals/AddProject';
+import Cookies from 'js-cookie';
+import { IUser } from '@/types/User';
+import { LightMode } from '@components/ui/color-mode';
 
 const SecondLayout = ({
   children,
@@ -20,44 +20,41 @@ const SecondLayout = ({
 
   useEffect(() => {
     try {
-      const userCookie = Cookies.get("user");
+      const userCookie = Cookies.get('user');
       if (userCookie) {
         const parsedUser = JSON.parse(userCookie) as IUser;
         // @ts-ignore
         setUser(parsedUser);
       }
     } catch (error) {
-      console.error("Failed to parse user cookie:", error);
+      console.error('Failed to parse user cookie:', error);
       setUser(null);
-      // Опционально: очистить битую куку
-      Cookies.remove("user");
+      Cookies.remove('user');
     }
   }, []);
   return (
     <Provider>
-      <LightMode>
-        <AddProjectModal />
-        <div className={styles.wrapper}>
-          <div className={styles.navbar}>
-            <Link href={"/"}>
-              <h5>УЮТ AR</h5>
-            </Link>
-            <div className={styles.account}>
-              <Image
-                className={styles.avatar}
-                src={user_img}
-                width={35}
-                height={35}
-                alt="account"
-              />
-              <p className={styles.fullName}>
-                {user?.name} {user?.surname}
-              </p>
-            </div>
+      <AddProjectModal />
+      <div className={styles.wrapper}>
+        <div className={styles.navbar}>
+          <Link href={'/'}>
+            <h5>УЮТ AR</h5>
+          </Link>
+          <div className={styles.account}>
+            <Image
+              className={styles.avatar}
+              src={user_img}
+              width={35}
+              height={35}
+              alt="account"
+            />
+            <p className={styles.fullName}>
+              {user?.name} {user?.surname}
+            </p>
           </div>
-          {children}
         </div>
-      </LightMode>
+        {children}
+      </div>
     </Provider>
   );
 };
